557c0813432741259bf2b50b1355f6792e08bbcc
Mohammed Bawa <mohdalal2008@gmail.com>
2026-03-06 05:43:19 +0530

    inital commit

---
+++ products.js
@@ -0,0 +1,295 @@
+
+
+
+
+
+
+
+
+
+const products = [
+
+{
+name: "From Dreams to Achievements Diary 🌙➡️🏆",
+price: "150₹",
+image: "asthetic diaries/quote diary copy.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},
+{
+name: "Blue Heart Dreams Diary 💙",
+price: "150₹",
+image: "asthetic diaries/heart diary.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},
+{
+name: "Coffee & Chaos Diary ☕🖤",
+price: "150₹",
+image: "asthetic diaries/coffee diary copy.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Modern Muse Diary ✍️",
+price: "150₹",
+image: "asthetic diaries/browb boho diary.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Earthy Brown Aesthetic Diary 🤎",
+price: "150₹",
+image: "asthetic diaries/brown aesthetic.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Hello 2026 Diary ✨📅",
+price: "150₹",
+image: "asthetic diaries/2026 diary.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Cherry On Top Moments Diary 🍒",
+price: "150₹",
+image: "asthetic diaries/cherry diary .jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Burnt Orange Vintage Diary 🧡",
+price: "150₹",
+image: "asthetic diaries/orange diary .jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Green Zen Modern Diary 🌿",
+price: "150₹",
+image: "asthetic diaries/green leaf.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Honeybee Hustle Diary 🐝🍯",
+price: "150₹",
+image: "asthetic diaries/honey bee diary.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Modern Vintage Soul Diary 🕰️✨",
+price: "150₹",
+image: "asthetic diaries/white aesthetic.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Bloom & Grace Floral Diary 🌸 ",
+price: "150₹",
+image: "asthetic diaries/floral diary copy.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},{
+name: "Avocado Vibes Diary 🥑✨",
+price: "150₹",
+image: "asthetic diaries/avacoda diary.jpg.jpeg",
+link: "aesthetic-diaries.html"
+},
+
+
+
+
+
+
+{
+name: "Green Beginnings 🌱",
+price: "150₹",
+image: "arabic diaries/green bismillah.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Pink Beginnings 🌸",
+price: "150₹",
+image: "arabic diaries/pink beginning.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Brown Reflections 🤎",
+price: "150₹",
+image: "arabic diaries/maroon curtain.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Ancient Letters 🏺",
+price: "150₹",
+image: "",
+link: "arabic diaries/half to half diary.jpg.jpeg"
+},
+{
+name: "Petals of Knowledge 🌺",
+price: "150₹",
+image: "arabic diaries/pink diary.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Growth of Knowledge 🌿",
+price: "150₹",
+image: "arabic diaries/green circle.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Circles of Wisdom 🔵",
+price: "150₹",
+image: "arabic diaries/so many circle diary.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Blue Hues 💙",
+price: "150₹",
+image: "arabic diaries/green and white diary.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Secret Letters 🔐",
+price: "150₹",
+image: "arabic diaries/circle of wisdom.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Midnight Script 🌙",
+price: "150₹",
+image: "arabic diaries/midnight diary.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Daylight Script ☀️",
+price: "150₹",
+image: "arabic diaries/black on white.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+{
+name: "Golden Year ✨",
+price: "150₹",
+image: "arabic diaries/2026 diary.jpg.jpeg",
+link: "arabic-diaries.html"
+},
+
+
+
+
+
+{
+name: "ا ب ت – Grow & Draw Book 🎨✨",
+price: "120₹",
+image: "drawing books/IMG_4043.PNG",
+link: "drawing-books.html"
+},
+{
+name: "١٢٣ Grow & Draw ✏️🔢⭐",
+price: "120₹",
+image: "drawing books/IMG_4044.PNG",
+link: "drawing-books.html"
+},
+{
+name: "Little Creatures Drawing Book 🐝🦋🎨",
+price: "120₹",
+image: "drawing books/IMG_4034.PNG",
+link: "drawing-books.html"
+},
+{
+name: "Cloud Hop Drawing Book ☁️🐰🖍️🌈",
+price: "120₹",
+image: "drawing books/IMG_4033.PNG",
+link: "drawing-books.html"
+},
+
+
+
+
+
+{
+name: "The Zodiac story🌠🌌",
+price: "170₹",
+image: "cutoms/ChatGPT Image Feb 2, 2026, 05_36_15 PM.png",
+link: "cutom-order.html"
+},
+{
+name: "The namecraft diary ✏️📝",
+price: "170₹",
+image: "cutoms/jamila diary.jpg.jpeg",
+link: "cutom-order.html"
+},
+
+{
+name: "The initial charm diary🫀💌",
+price: "170₹",
+image: "cutoms/alif.jpg.jpeg",
+link: "cutom-order.html"
+},
+
+{
+name: "The name craft diary ✏️📝",
+price: "170₹",
+image: "cutoms/ChatGPT Image Feb 2, 2026, 05_42_48 PM.png",
+link: "cutom-order.html"
+},
+
+{
+name: "The initial charm diary🫀💌",
+price: "170₹",
+image: "cutoms/ChatGPT Image Feb 2, 2026, 05_44_17 PM.png",
+link: "cutom-order.html"
+},
+
+
+
+
+{
+name: "🌙 My Ramadan Activity Kit ✨",
+price: "300₹",
+image: "combos/WhatsApp Image 2026-02-10 at 12.47.22 AM 06.jpeg",
+link: "combos.html"
+},
+{
+name: "🌟 Zodiac Story Combo 🌟",
+price: "2200₹",
+image: "combos/ChatGPT Image Feb 10, 2026, 11_13_18 AM.png",
+link: "combos.html"
+},
+
+
+
+
+];
+
+
+
+
+
+
+
+
+
+const searchInput = document.getElementById("globalSearch");
+const resultsBox = document.getElementById("searchResults");
+
+searchInput.addEventListener("input", () => {
+
+const query = searchInput.value.toLowerCase();
+resultsBox.innerHTML = "";
+
+if(query.length === 0){
+resultsBox.style.display = "none";
+return;
+}
+
+const filtered = products.filter(product =>
+product.name.toLowerCase().includes(query)
+);
+
+filtered.forEach(product => {
+
+const item = document.createElement("div");
+item.classList.add("search-item");
+
+item.innerHTML = `
+<img src="${product.image}">
+<div>
+<p>${product.name}</p>
+<span>${product.price}</span>
+</div>
+`;
+
+item.onclick = () => {
+window.location.href = product.link;
+};
+
+resultsBox.appendChild(item);
+
+});
+
+resultsBox.style.display = "block";
+
+});
